88b145838261a0f515d79907d2e30923e3144ad0
solrakcs <carlitoscs99@gmail.com>
2019-09-01 18:24:43 -0400

    Done step 62

---
+++ Del101.py
@@ -0,0 +1,33 @@
+from pygameWindow import PYGAME_WINDOW
+import random
+
+x = 540
+y = 360
+
+def Perturb_Circle_Position(): #I am not gonna include a velocity. 1 works well for me
+	global x, y
+
+	fourSidedDieRoll = random.randint(1, 4)
+	
+	if fourSidedDieRoll == 1:
+		x -= 1
+	elif fourSidedDieRoll == 2:
+		x += 1
+	elif fourSidedDieRoll == 3:
+		y -= 1
+	else:
+		y += 1
+
+
+
+pygameWindow = PYGAME_WINDOW()
+
+print(pygameWindow)
+
+while True:
+	pygameWindow.Prepare(pygameWindow)
+	Perturb_Circle_Position()
+	pygameWindow.Draw_Black_Circle(x,y)
+	pygameWindow.Reveal()
+
+
