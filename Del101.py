030175bf864e8f8057b0ea53b39bbd0c24865180
solrakcs <carlitoscs99@gmail.com>
2019-09-01 18:36:08 -0400

    Not recognising the libraries

--- Del101.py
+++ Del101.py
@@ -1,33 +1,38 @@
-from pygameWindow import PYGAME_WINDOW
-import random
-
-x = 540
-y = 360
-
-def Perturb_Circle_Position(): #I am not gonna include a velocity. 1 works well for me
-	global x, y
-
-	fourSidedDieRoll = random.randint(1, 4)
-	
-	if fourSidedDieRoll == 1:
-		x -= 1
-	elif fourSidedDieRoll == 2:
-		x += 1
-	elif fourSidedDieRoll == 3:
-		y -= 1
-	else:
-		y += 1
+import sys
+sys.path.insert(0, '..')
+import Leap
 
 
+#from pygameWindow import PYGAME_WINDOW
+#import random
 
-pygameWindow = PYGAME_WINDOW()
+#x = 540
+#y = 360
 
-print(pygameWindow)
+#def Perturb_Circle_Position(): #I am not gonna include a velocity. 1 works well for me
+#	global x, y
+#
+#	fourSidedDieRoll = random.randint(1, 4)
+#	
+#	if fourSidedDieRoll == 1:
+#		x -= 1
+#	elif fourSidedDieRoll == 2:
+#		x += 1
+#	elif fourSidedDieRoll == 3:
+#		y -= 1
+#	else:
+#		y += 1
+#
+#
+#
+#pygameWindow = PYGAME_WINDOW()
 
-while True:
-	pygameWindow.Prepare(pygameWindow)
-	Perturb_Circle_Position()
-	pygameWindow.Draw_Black_Circle(x,y)
-	pygameWindow.Reveal()
+#print(pygameWindow)
+
+#while True:
+#	pygameWindow.Prepare(pygameWindow)
+#	Perturb_Circle_Position()
+#	pygameWindow.Draw_Black_Circle(x,y)
+#	pygameWindow.Reveal()
 
 
