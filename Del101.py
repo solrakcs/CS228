328ecbc895eb6e29ef2d8cdd92179bc114119072
solrakcs <carlitoscs99@gmail.com>
2019-09-02 00:31:30 -0400

    Done step 78

--- Del101.py
+++ Del101.py
@@ -2,11 +2,11 @@ import sys
 sys.path.insert(0, "..")
 import Leap
 
-#from pygameWindow import PYGAME_WINDOW
-#import random
+from pygameWindow import PYGAME_WINDOW
+import random
 
-#x = 540
-#y = 360
+x = 540
+y = 360
 
 #def Perturb_Circle_Position(): #I am not gonna include a velocity. 1 works well for me
 #	global x, y
@@ -21,31 +21,39 @@ import Leap
 #		y -= 1
 #	else:
 #		y += 1
-#
+
 
 def Handle_Frame():
+	global x, y
+
 	hand = frame.hands[0]
 	print (hand)
 	fingers = hand.fingers
 	indexFingerList = fingers.finger_type(0)
 	indexFinger = indexFingerList[0]
-	print(indexFinger)
-#
-#
-#pygameWindow = PYGAME_WINDOW()
+	distalPhalanx = indexFinger.bone(3)
+	print(distalPhalanx)
+	distalPhalanx = indexFinger.bone(3)
+	tip = distalPhalanx.next_joint
+	print(tip)
 
-#print(pygameWindow)
+	x = tip[0]
+	y = tip[1]
+
+pygameWindow = PYGAME_WINDOW()
+
+print(pygameWindow)
 
 controller = Leap.Controller()
 
 while True:
-#	pygameWindow.Prepare(pygameWindow)
-#	Perturb_Circle_Position()
-#	pygameWindow.Draw_Black_Circle(x,y)
+	pygameWindow.Prepare(pygameWindow)
+	#Perturb_Circle_Position()
+	pygameWindow.Draw_Black_Circle(int(x),int(y))
 	frame = controller.frame()
 	if not (frame.hands.is_empty > 0):
 		 Handle_Frame()
 
-#	pygameWindow.Reveal()
+	pygameWindow.Reveal()
 
 
