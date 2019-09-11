50f5ac5f45f4e4ee30081760335d2be9a9c94b54
solrakcs <carlitoscs99@gmail.com>
2019-09-11 09:08:23 -0400

    Step 4 done

--- Derivable.py
+++ Derivable.py
@@ -9,3 +9,69 @@ class DERIVABLE:
 		self.x = 540
 		self.y = 360
 		self.befValue = 0
+
+
+
+	def Handle_Vector_From_Leap(v):
+	global xMin, xMax, yMin, yMax
+	global x, y
+
+
+	x = v[0]
+	y = v[2]
+
+	if (x < xMin):
+		xMin = x
+	if (x > xMax):
+		xMax = x
+	if (y < yMin):
+		yMin = y
+	if (y > yMax):
+		yMax = y
+	
+	def Handle_Bone(b):
+		global bone
+		global base, tip
+	
+		bone = finger.bone(b)
+		base = bone.prev_joint
+		tip = bone.next_joint
+		Handle_Vector_From_Leap(base)
+		pygameXBase = Scale(x, xMin, xMax, 0, 1080)
+		pygameYBase = Scale(y, yMin, yMax, 0, 720)
+		Handle_Vector_From_Leap(tip)
+		pygameXTip = Scale(x, xMin, xMax, 0, 1080)
+		pygameYTip = Scale(y, yMin, yMax, 0, 720)
+		pygameWindow.Draw_Black_Line(pygameXBase, pygameYBase, pygameXTip, pygameYTip, b)
+		
+
+
+
+	def Handle_Finger(finger):
+		for b in range(4):
+			Handle_Bone(b)
+				
+	
+	
+	def Handle_Frame():
+		global x, y
+		global finger
+	
+		hand = frame.hands[0]
+		fingers = hand.fingers
+		length = len(fingers) 
+		for i in range(length):
+			finger = fingers[i]
+			Handle_Finger(finger)
+	
+	
+	def Scale(value, minValue, maxValue, newMinValue, newMaxValue):
+	
+		global befValue
+	
+		if maxValue == minValue:
+			return befValue
+	
+		percentage_scaling = (value - minValue) / (maxValue - minValue)
+		befValue = ((newMaxValue - newMinValue) * percentage_scaling) + newMinValue
+		return befValue	
