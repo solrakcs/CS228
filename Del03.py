a01fd27a8ae1abea61b75f026a9b36b000d40aea
solrakcs <carlitoscs99@gmail.com>
2019-09-14 13:50:25 -0400

    Step 5 done

--- Del03.py
+++ Del03.py
@@ -6,6 +6,5 @@ from Derivable import DERIVABLE
 import random
 import pygame
 
-new_derivable = DERIVABLE()
-
-
+new_derivable = DERIVABLE(PYGAME_WINDOW(), Leap.Controller(), 540, 360, 0, 1000.0, -1000.0, 1000.0, -1000.0)
+new_derivable.Run_Forever()
--- Derivable.py
+++ Derivable.py
@@ -1,33 +1,43 @@
 import sys
 sys.path.insert(0, "..")
 import Leap
-from pygameWindow_Del03 import PYGAME_WINDOW
+from pygameWindow import PYGAME_WINDOW
 import random
 import pygame
 
 class DERIVABLE:
 
-	def __init__(self):
+	global x, y, befValue, xMin, xMax, yMin, yMax, pygameWindow, controller
 
-		self.pygameWindow = PYGAME_WINDOW()
-		self.controller = Leap.Controller()
-		
-		self.x = 540
-		self.y = 360
+	x = 540
+	y = 360
+	befValue = 0
+	xMin = 1000.0
+	xMax = -1000.0
+	yMin = 1000.0 
+	yMax = -1000.0
+	pygameWindow = PYGAME_WINDOW()
+	controller = Leap.Controller()
 
-		self.befValue = 0
 
-		self.xMin = 1000.0
-		self.xMax = -1000.0
-		self.yMin = 1000.0 
-		self.yMax = -1000.0
+	def __init__(self, pygameWindow, controller, x, y, befValue, xMin, xMax, yMin, yMax):
+
+		self.pygameWindow = pygameWindow
+		self.controller = controller
+		self.x = x
+		self.y = y
+		self.befValue = befValue
+		self.xMin = xMin
+		self.xMax = xMax
+		self.yMin = yMin
+		self.yMax = yMax
 
 
 	def Handle_Vector_From_Leap(self, v):
 		global xMin, xMax, yMin, yMax
 		global x, y
-
-
+	
+	
 		x = v[0]
 		y = v[2]
 
@@ -40,6 +50,17 @@ class DERIVABLE:
 		if (y > yMax):
 			yMax = y
 
+	def Scale(self, value, minValue, maxValue, newMinValue, newMaxValue):
+	
+		global befValue
+	
+		if maxValue == minValue:
+			return befValue
+	
+		percentage_scaling = (value - minValue) / (maxValue - minValue)
+		befValue = ((newMaxValue - newMinValue) * percentage_scaling) + newMinValue
+		return befValue	
+
 	def Handle_Bone(self, b):
 		global bone
 		global base, tip
@@ -47,12 +68,12 @@ class DERIVABLE:
 		bone = finger.bone(b)
 		base = bone.prev_joint
 		tip = bone.next_joint
-		Handle_Vector_From_Leap(base)
-		pygameXBase = Scale(x, xMin, xMax, 0, 1080)
-		pygameYBase = Scale(y, yMin, yMax, 0, 720)
-		Handle_Vector_From_Leap(tip)
-		pygameXTip = Scale(x, xMin, xMax, 0, 1080)
-		pygameYTip = Scale(y, yMin, yMax, 0, 720)
+		self.Handle_Vector_From_Leap(base)
+		pygameXBase = self.Scale(x, xMin, xMax, 0, 1080)
+		pygameYBase = self.Scale(y, yMin, yMax, 0, 720)
+		self.Handle_Vector_From_Leap(tip)
+		pygameXTip = self.Scale(x, xMin, xMax, 0, 1080)
+		pygameYTip = self.Scale(y, yMin, yMax, 0, 720)
 		pygameWindow.Draw_Black_Line(pygameXBase, pygameYBase, pygameXTip, pygameYTip, b)
 		
 	
@@ -60,7 +81,7 @@ class DERIVABLE:
 	
 	def Handle_Finger(self, finger):
 		for b in range(4):
-			Handle_Bone(b)
+			self.Handle_Bone(b)
 				
 	
 	
@@ -73,34 +94,21 @@ class DERIVABLE:
 		length = len(fingers) 
 		for i in range(length):
 			finger = fingers[i]
-			Handle_Finger(finger)
+			self.Handle_Finger(finger)
 	
-	
-	def Scale(self, value, minValue, maxValue, newMinValue, newMaxValue):
-	
-		global befValue
-	
-		if maxValue == minValue:
-			return befValue
-	
-		percentage_scaling = (value - minValue) / (maxValue - minValue)
-		befValue = ((newMaxValue - newMinValue) * percentage_scaling) + newMinValue
-		return befValue
 
 	def Run_Forever(self):
 
+		global frame
+
 		while True:
 
-			Run_Once()
-	
-
-	def Run_Once(self):
-		pygameWindow.Prepare(pygameWindow)
-		frame = controller.frame()
-		for event in pygame.event.get(): #With this for loop pygame window do not crash
-			if event.type == pygame.QUIT:
-				sys.exit(0)
-		if not (frame.hands.is_empty > 0):
-			 Handle_Frame()
-		 
-		pygameWindow.Reveal()
+			pygameWindow.Prepare(pygameWindow)
+			frame = controller.frame()
+			for event in pygame.event.get(): #With this for loop pygame window do not crash
+				if event.type == pygame.QUIT:
+					sys.exit(0)
+			if not (frame.hands.is_empty > 0):
+				 self.Handle_Frame()
+				 
+			pygameWindow.Reveal()
