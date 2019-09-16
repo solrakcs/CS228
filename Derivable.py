417b5e83a3c34714626a4401ca0cbc24552ce8be
solrakcs <carlitoscs99@gmail.com>
2019-09-16 13:11:51 -0400

    Step 35 Done

--- Derivable.py
+++ Derivable.py
@@ -5,6 +5,7 @@ from pygameWindow_Del03 import PYGAME_WINDOW
 import random
 import pygame
 import numpy as np
+import pickle
 
 class DERIVABLE:
 
@@ -48,11 +49,11 @@ class DERIVABLE:
 		self.befValue = ((newMaxValue - newMinValue) * percentage_scaling) + newMinValue
 		return self.befValue	
 
-	def Handle_Bone(self, b, c):
+	def Handle_Bone(self, i, j, c):
 		global bone
 		global base, tip
 	
-		bone = finger.bone(b)
+		bone = finger.bone(j)
 		base = bone.prev_joint
 		tip = bone.next_joint
 		self.Handle_Vector_From_Leap(base)
@@ -61,14 +62,24 @@ class DERIVABLE:
 		self.Handle_Vector_From_Leap(tip)
 		pygameXTip = self.Scale(self.x, self.xMin, self.xMax, 0, 1080)
 		pygameYTip = self.Scale(self.y, self.yMin, self.yMax, 0, 720)
-		self.pygameWindow.Draw_Line(pygameXBase, pygameYBase, pygameXTip, pygameYTip, b, c)
+		self.pygameWindow.Draw_Line(pygameXBase, pygameYBase, pygameXTip, pygameYTip, i, c)
+
+		if self.Recording_Is_Ending() == True:
+				self.gestureData[i,j,0] = base[0]
+ 				self.gestureData[i,j,1] = base[1]
+ 				self.gestureData[i,j,2] = base[2]
+ 				self.gestureData[i,j,3] = tip[0]
+ 				self.gestureData[i,j,4] = tip[1]
+ 				self.gestureData[i,j,5] = tip[2]
 		
 	
 	
 	
 	def Handle_Finger(self, finger, c):
-		for b in range(4):
-			self.Handle_Bone(b, c)
+		for i in range(5):
+			for j in range(4):
+				self.Handle_Bone(i, j, c)
+
 				
 	
 	
@@ -93,7 +104,8 @@ class DERIVABLE:
 			if(self.currentNumberofHands == 2):
 				self.Handle_Finger(finger, 2)
 			if self.Recording_Is_Ending() == True:
-				print(self.gestureData[0,3,3:6])
+				print(self.gestureData)
+				self.Save_Gesture()	
 				break
 
 	
@@ -125,3 +137,9 @@ class DERIVABLE:
 	def Recording_Is_Ending(self):
 		if(self.currentNumberofHands == 1 and self.previousNumberofHands == 2):
 			return True
+
+	def Save_Gesture(self):
+		
+		pickle_out = open("C:\\Users\\ruths\\Desktop\\2019_UVM_CS228_Castrejon_Carlos_Deliverable 3\\LeapSDK\\lib\\CS228\\userData\\gesture.p","wb")
+		pickle.dump(self.gestureData, pickle_out)
+		pickle_out.close()
