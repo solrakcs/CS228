bc6a8876f09ce90165cd268d887e6048284ba8ef
solrakcs <carlitoscs99@gmail.com>
2019-09-16 14:26:30 -0400

    Step 47 in process

--- Reader.py
+++ Reader.py
@@ -3,9 +3,10 @@ import os
 
 class READER:
 
-	def __init__(self):
+	def __init__(self, pygameWindow):
 
 		self.Determine_Num_Gestures()
+		self.pygameWindow = pygameWindow
 
 	def Determine_Num_Gestures(self):
 
@@ -14,6 +15,20 @@ class READER:
 
 	def Print_Gestures(self):
 		
+		for i in range(0, self.numGestures):
+			pickle_in = open("C:\\Users\\ruths\\Desktop\\2019_UVM_CS228_Castrejon_Carlos_Deliverable 3\\LeapSDK\\lib\\CS228\\userData\\gesture"+str(i)+".p", "rb")
+			gestureData = pickle.load(pickle_in)
+			print("\n")
+			print (gestureData)
+
+	def Draw_Gestures(self):
+		
+		while True:
+
+			self.Draw_Each_Gesture_Once()
+
+	def Draw_Each_Gesture_Once():
+
 		for i in range(0, self.numGestures):
 			pickle_in = open("C:\\Users\\ruths\\Desktop\\2019_UVM_CS228_Castrejon_Carlos_Deliverable 3\\LeapSDK\\lib\\CS228\\userData\\gesture"+str(i)+".p", "rb")
 			gestureData = pickle.load(pickle_in)
