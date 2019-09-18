d3b9d855180f09da7f4fd8990cd4a9849fd39b78
solrakcs <carlitoscs99@gmail.com>
2019-09-17 20:50:11 -0400

    Step 24 Done

--- Predict.py
+++ Predict.py
@@ -3,10 +3,12 @@ from knn import KNN
 
 
 knn = KNN()
-
 knn.Load_Dataset('iris.csv')
+
 x = knn.data[:, 0]
 y = knn.data[:, 1]
+trainX = knn.data[::2, 0:2]
+trainy = knn.target[::2]
 plt.figure()
-plt.scatter(x, y, c=knn.target)
+plt.scatter(trainX[:, 0], trainX[:, 1], c=trainy)
 plt.show()
