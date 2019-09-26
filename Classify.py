d6b1844384ec2c097977be067feb90503708724d
solrakcs <carlitoscs99@gmail.com>
2019-09-26 09:16:44 -0400

    Step 35 Done

--- Classify.py
+++ Classify.py
@@ -2,36 +2,44 @@ import numpy as np
 import pickle
 
 
+pickle_in = open("C:\\Users\\ruths\\Desktop\\2019_UVM_CS228_Castrejon_Carlos_Deliverable 4\\LeapSDK\\lib\\CS228\\userData\\train8.dat.p", "rb")
+train8 = pickle.load(pickle_in)
+pickle_in = open("C:\\Users\\ruths\\Desktop\\2019_UVM_CS228_Castrejon_Carlos_Deliverable 4\\LeapSDK\\lib\\CS228\\userData\\train9.dat.p", "rb")
+train9 = pickle.load(pickle_in)
+pickle_in = open("C:\\Users\\ruths\\Desktop\\2019_UVM_CS228_Castrejon_Carlos_Deliverable 4\\LeapSDK\\lib\\CS228\\userData\\test8.dat.p", "rb")
+test8 = pickle.load(pickle_in)
+pickle_in = open("C:\\Users\\ruths\\Desktop\\2019_UVM_CS228_Castrejon_Carlos_Deliverable 4\\LeapSDK\\lib\\CS228\\userData\\test9.dat.p", "rb")
+test9 = pickle.load(pickle_in)
 
-def Main():
 
-	pickle_in = open("C:\\Users\\ruths\\Desktop\\2019_UVM_CS228_Castrejon_Carlos_Deliverable 4\\LeapSDK\\lib\\CS228\\userData\\train8.dat.p", "rb")
-	train8 = pickle.load(pickle_in)
-	print(train8)
-	pickle_in = open("C:\\Users\\ruths\\Desktop\\2019_UVM_CS228_Castrejon_Carlos_Deliverable 4\\LeapSDK\\lib\\CS228\\userData\\train9.dat.p", "rb")
-	train9 = pickle.load(pickle_in)
-	print(train9)
-	pickle_in = open("C:\\Users\\ruths\\Desktop\\2019_UVM_CS228_Castrejon_Carlos_Deliverable 4\\LeapSDK\\lib\\CS228\\userData\\test8.dat.p", "rb")
-	test8 = pickle.load(pickle_in)
-	print(test8)
-	pickle_in = open("C:\\Users\\ruths\\Desktop\\2019_UVM_CS228_Castrejon_Carlos_Deliverable 4\\LeapSDK\\lib\\CS228\\userData\\test9.dat.p", "rb")
-	test9 = pickle.load(pickle_in)
-	print(test9)
 
 def ReshapeData(set1,set2):
 	X = np.zeros((2000,5*4*6),dtype='f')
+	y = np.zeros(2000, dtype = 'f')
 	for row in range(0,1000):
+		y[row] = 8
+		y[row+1000] = 9
 		col = 0
 		for j in range(0,5):
 			for k in range(0,4):
 				for m in range(0,6):
 					X[row,col] = set1[j,k,m,row]
+					X[row+1000, col] = set2[j,k,m,row]
 					col = col + 1
-	return X
-	trainX = ReshapeData(trainM,trainN)
-	print trainX
-	print trainX.shape
+
+	return X, y
 
 
+trainX, trainy = ReshapeData(train8, train9)
+#print trainX
+#print trainX.shape
+#print trainy
+#print trainy.shape
 
-Main()
+testX, testy = ReshapeData(test8, test9)
+print testX
+print testX.shape
+print testy
+print testy.shape
+
+
